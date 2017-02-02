bd3a44abe9bd3d44bdebb967a71315af49d12afc
Chronophylos <admiral.chronophylos@web.de>
2017-02-02 08:34:57 +0100

    reworked arguments

--- AdressbuchClientConsole/Program.cs
+++ AdressbuchClientConsole/Program.cs
@@ -4,49 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Adressbuch
-{
+namespace Adressbuch {
     // Das ist der Client
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
             // Standardhost ist localhost
             string host = "127.0.0.1";
 
             // Standardport ist 55555
             int port = 55555;
 
-            // Eventuelle Argumente durchlaufen
-            if (args.Length > 1)
-            {
-                foreach (string arg in args)
-                {
-                    // Argumente: /host:10.2.210.21 /port:12345
-                    char[] separator = { ':' };
-                    string[] argument = arg.Split(separator);
+            if (args.Length < 1) {
+                Console.WriteLine("Usage: host:port");
+                Environment.Exit(22);
+            }
 
-                    switch (argument[0])
-                    {
-                        case "/port":
-                            port = Convert.ToInt32(argument[1]);
-                            break;
+            host = args[0].Split(':')[0];
+            port = Convert.ToInt32(args[0].Split(':')[1]);
 
-                        case "/host":
-                            port = Convert.ToInt32(argument[1]);
-                            break;
-
-                        default:
-                            break;
-                    } // Ende switch
-                } // Ende foreach
-            } // Ende if
-
-
-            int eingabe = 0;
-
-            do
-            {
+            int eingabe;
+            do {
                 // Controller-Objekt erstellen
                 ControllerClient controller = new ControllerClient(host, port);
                 eingabe = controller.start();
