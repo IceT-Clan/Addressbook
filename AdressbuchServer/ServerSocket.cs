2795f72b3dd6cafeb3342d14368d2c6782687ee5
Chronophylos <admiral.chronophylos@web.de>
2017-02-02 08:20:07 +0100

    reworked ServerSocket

--- AdressbuchServer/ServerSocket.cs
+++ AdressbuchServer/ServerSocket.cs
@@ -4,47 +4,35 @@ using System.Net.Sockets;
 using System.Threading;
 using __ClientSocket__;
 
-namespace __ServerSocket__
-{
-    class ServerSocket
-    {
-        private int port = 0;
-        private Socket serverSocket = null;
+namespace __ServerSocket__ {
+    class ServerSocket {
+        private int port;
+        private Socket serverSocket;
 
-        public ServerSocket(int port)
-        {
+        public ServerSocket(int port) {
             this.port = port;
             serverSocket = new Socket(AddressFamily.InterNetwork,
                                          SocketType.Stream,
                                          ProtocolType.Tcp);
-            
-            IPAddress hostIP = (Dns.Resolve(IPAddress.Any.ToString())).AddressList[0];
+
+            IPAddress hostIP = (Dns.GetHostEntry(IPAddress.Any.ToString())).AddressList[0];
             IPEndPoint ep = new IPEndPoint(hostIP, port);
 
             // MessageBox.Show(ep.ToString());
 
-            try
-            {
+            try {
                 serverSocket.Bind(ep);
                 serverSocket.Listen(1);
-            }
-            catch ( Exception ex)
-            {
+            } catch (Exception ex) {
                 // MessageBox.Show(ex.Message);
                 serverSocket.Close();
                 return;
             }
         }
 
-        public Socket accept()
-        {
-            return serverSocket.Accept();
-        }
+        public Socket accept() => serverSocket.Accept();
 
-        public void close()
-        {
-            serverSocket.Close();
-        }
+        public void close() => serverSocket.Close();
 
     }
 }
